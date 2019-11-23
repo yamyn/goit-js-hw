4d67b26862167a26c3a0a67aa068d8778c62fa3a
Yaroslav Myn <ricky.month@gmail.com>
2019-11-23 22:57:04 +0200

    Create module 5, made task 1-2

--- module-4/js/task-2.js
+++ module-4/js/task-2.js
@@ -1,32 +1,36 @@
-'use strict';
+// 'use strict';
 
-const inventory = {
-  items: ['Монорельса', 'Фильтр'],
-  add(itemName) {
-    console.log(`Adding ${itemName} to inventory`);
+// const inventory = {
+//   items: ['Монорельса', 'Фильтр'],
+//   add(itemName) {
+//     console.log(`Adding ${itemName} to inventory`);
 
-    this.items.push(itemName);
-  },
-  remove(itemName) {
-    console.log(`Removing ${itemName} from inventory`);
+//     this.items.push(itemName);
+//   },
+//   remove(itemName) {
+//     console.log(`Removing ${itemName} from inventory`);
 
-    this.items = this.items.filter(item => item !== itemName);
-  },
-};
+//     this.items = this.items.filter(item => item !== itemName);
+//   },
+// };
 
-const invokeInventoryAction = function(itemName, action) {
-  console.log(`Invoking action on ${itemName}`);
-  action(itemName);
-};
+// const invokeInventoryAction = function(itemName, action) {
+//   console.log(`Invoking action on ${itemName}`);
+//   action(itemName);
+// };
 
-invokeInventoryAction('Аптечка', inventory.add.bind(inventory));
-// Invoking action on Аптечка
-// Adding Аптечка to inventory
+// invokeInventoryAction('Аптечка', inventory.add.bind(inventory));
+// // Invoking action on Аптечка
+// // Adding Аптечка to inventory
 
-console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
+// console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
 
-invokeInventoryAction('Фильтр', inventory.remove.bind(inventory));
-// Invoking action on Фильтр
-// Removing Фильтр from inventory
+// invokeInventoryAction('Фильтр', inventory.remove.bind(inventory));
+// // Invoking action on Фильтр
+// // Removing Фильтр from inventory
 
-console.log(inventory.items); // ['Монорельса', 'Аптечка']
+// console.log(inventory.items); // ['Монорельса', 'Аптечка']
+class Student {
+  constructor() {}
+}
+console.log(typeof Student === 'function');
