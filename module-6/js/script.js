249ccfac56bd729a16a37b3f2a7299fa2c3e3b51
Yaroslav Myn <ricky.month@gmail.com>
2019-11-30 22:22:08 +0200

    made task 7

--- module-6/js/script.js
+++ module-6/js/script.js
@@ -86,9 +86,72 @@ const users = [
   },
 ];
 
-const getUserNames = users => {
-  return users.map(user => user.name);
-};
+// //==================TASK 1
+// {
+//   const getUserNames = users => {
+//     return users.map(user => user.name);
+//   };
 
-console.log(getUserNames(users));
-// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+//   console.log(getUserNames(users));
+//   // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+// }
+
+// //==================TASK 2
+// {
+//   const getUsersWithEyeColor = (users, color) => {
+//     return users.filter(user => user.eyeColor === color);
+//   };
+
+//   console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+// }
+
+// //==================TASK 3
+// {
+//   const getUsersWithGender = (users, gender) => {
+//     const resultGenderFiltr = users.filter(user => user.gender === gender);
+//     const userNamesSomeGender = resultGenderFiltr.map(user => user.name);
+//     return userNamesSomeGender;
+//   };
+
+//   console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+// }
+
+// //==================TASK 4
+// {
+//   const getInactiveUsers = users => {
+//     return users.filter(user => !user.isActive);
+//   };
+
+//   console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
+// }
+
+// //==================TASK 5
+// {
+//   const getUserWithEmail = (users, email) => {
+//     return users.find(user => user.email === email);
+//   };
+
+//   console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
+//   console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
+// }
+
+//==================TASK 6
+{
+  const getUsersWithAge = (users, min, max) => {
+    return users.filter(user => user.age >= min && user.age <= max);
+  };
+
+  console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+
+  console.log(getUsersWithAge(users, 30, 40));
+  // [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
+}
+
+//==================TASK 7
+{
+  const calculateTotalBalance = users => {
+    // твой код
+  };
+
+  console.log(calculateTotalBalance(users)); // 20916
+}
