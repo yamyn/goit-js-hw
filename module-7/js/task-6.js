98be1ff4fa397b5823e240bfb7ec3ad3888425d3
Yaroslav Myn <ricky.month@gmail.com>
2019-12-19 09:44:36 +0200

    FIxed task 6

--- module-7/js/task-6.js
+++ module-7/js/task-6.js
@@ -6,18 +6,11 @@ const validInput = document.querySelector('#validation-input');
 validInput.addEventListener('change', validationText.bind(validInput));
 
 function validationText() {
-  console.log('this: ' + this);
+  this.classList.remove('invalid');
   const lengthValue = this.value.split('').length;
   const validation = this.dataset.length;
-  if (lengthValue < validation) {
-    if (this.classList.contains('valid')) {
-      this.classList.remove('valid');
-    }
-    this.classList.toggle('invalid');
-    return;
-  }
-  if (this.classList.contains('invalid')) {
-    this.classList.remove('invalid');
-  }
   this.classList.toggle('valid');
+  if (lengthValue < validation) {
+    this.classList.replace('valid', 'invalid');
+  }
 }
